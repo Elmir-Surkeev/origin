3ebf4dd03c0fd1c03aa7428279d031d89a3c1aee
Elmirbek Surkeev <elmirsurkeev@gmail.com>
2024-04-05 00:38:31 +0600

    first commit

---
+++ src/ForExample.java
@@ -0,0 +1,14 @@
+public class ForExample {
+    public static void main(String[] args) {
+        for (int value = 1; value < 83; value++){
+            if (value == 5){
+                continue;
+                //break;
+            }
+            System.out.println(value);
+        }
+        for (int value = 10; value > 0; value -- ){
+            System.out.println("hey");
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        int value = 0;
+        do {
+            System.out.println("hey");
+            value++;
+        }while (value < 10);
+    }
+}
