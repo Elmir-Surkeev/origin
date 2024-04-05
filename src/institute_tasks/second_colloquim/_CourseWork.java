0d45734aeaabff6920f066fbc89b4770811c0b1d
Elmirbek Surkeev <elmirsurkeev@gmail.com>
2024-04-05 11:31:44 +0600

    added file course work

---
+++ src/institute_tasks/second_colloquim/_CourseWork.java
@@ -0,0 +1,7 @@
+package institute_tasks.second_colloquim;
+
+public class _CourseWork {
+    public static void main(String[] args) {
+        System.out.println("Скоро начнем делать курсовую работу");
+    }
+}
